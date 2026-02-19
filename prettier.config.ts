55e2e1325bfca075ea0bdd1a151f402cca006bfa
Keanred <47241653+Keanred@users.noreply.github.com>
2026-02-19 22:27:25 +0200

    initial commit

---
+++ prettier.config.ts
@@ -0,0 +1,14 @@
+import { type Config } from "prettier";
+
+const config: Config = {
+  trailingComma: "all",
+  tabWidth: 2,
+  semi: true,
+  printWidth: 120,
+  singleQuote: true,
+  bracketSpacing: true,
+  endOfLine: "lf",
+  plugins: ["prettier-plugin-organize-imports"],
+};
+
+export default config;
